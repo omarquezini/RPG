52767442ac16f2ea31ef269d776f7f73914484fd
Rafael Marquezini Silva <rafael.marquezini.silva@escola.pr.gov.br>
2025-08-25 14:13:05 +0000

    fasendo uns textos ai.

---
+++ numSEI.py
@@ -0,0 +1,14 @@
+print("[]========================[]")
+print("[]                        []")
+print("[]     *O_MARQUEZINI*     []")
+print("[]      a aula final.     []")
+print("[]                        []")
+print("[]========================[]")
+
+print("")
+print("Um monstro terrivel a anos")
+print("atrai pesouas para uma")
+print("caverna misteriosa.")
+print("")
+print("missão: matar a criatura! ")
+
